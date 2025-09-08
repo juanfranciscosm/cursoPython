490e191ffdf9ce7db026efdbe07920f8cec8c19d
Juan Francisco Sanchez <jfranciscosm99@gmail.com>
2025-09-08 17:30:10 -0500

    clase 5

---
+++ clase5.py
@@ -0,0 +1,30 @@
+# --- TIPOS DE DATOS ---
+# los tipos de datos son la clase o class
+
+# --- ENTEROS ---
+x = 10
+print(type(x))
+
+# --- FLOTANTE ---
+y = 5.678
+print(type(y))
+
+z = 1.2e6 #notacion cientifica e = *10^. Puede ser e o E
+a = 1.2e-6
+print(z)
+print(a)
+
+# la notacion cientifica se usa cuando el numero es muy grando o muy pequeno
+
+# --- BOOLEAN ---
+is_true = True
+is_false = False
+print(is_true)
+print(type(is_true))
+
+# --- OPERACIONES MATEMATICAS ---
+# Si usamos un flotante en la suma, aunque tengamos un entero, el resultado sera un flotante
+print(x+y)
+print(x+x)
+print(y+y)
+
