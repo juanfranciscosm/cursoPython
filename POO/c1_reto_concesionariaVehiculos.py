05ad458f5664ddb2276be2c66d4ea53b8f577572
Juan Francisco Sanchez <jfranciscosm99@gmail.com>
2025-09-10 12:25:32 -0500

    clase ejemplo de administrador de biblioteca + reto por completar

---
+++ POO/c1_reto_concesionariaVehiculos.py
@@ -0,0 +1,6 @@
+# ------------------ RETO CONCESIONARIA DE VEHICULOS ----------------------
+'''
+Desarrolla una concesionaria de vehículos en la cual se puedan gestionar las compras y ventas de vehículos. 
+Un usuario podrá ver los vehículos disponibles, su precio y realizar la compra de uno. Aplica los conceptos de 
+programación orientada a objetos vistos en este ejercicio.
+'''
